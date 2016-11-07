08a3b2033cd92477cbb591652790ee9947da1003
Matej11 <hoorvaat95@gmail.com>
2016-11-07 19:04:04 +0100

    Add files via upload

---
+++ Matej/Assets/Skripte/glavniMeni.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class glavniMeni : MonoBehaviour {
+
+    public Canvas quitMenu;
+    public Button startText;
+    public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+
+        quitMenu = quitMenu.GetComponent<Canvas>();
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+        quitMenu.enabled = false;
+	}
+	
+	public void ExitPress() //ko pritisnemo na Izhod v meniju
+    {
+        quitMenu.enabled = true;
+        startText.enabled = false; 
+        exitText.enabled = false;
+    }
+
+    public void NoPress() //ko pritisnemo Izhod -> Ne
+    {
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
