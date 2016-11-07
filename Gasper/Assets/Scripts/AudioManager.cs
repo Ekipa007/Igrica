21c7b3fadcab83e25c5950bfe7489c0d67002331
Gasprck <rencof.gasper@gmail.com>
2016-11-07 20:41:43 +0100

    Assets

---
+++ Gasper/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+    float masterVolumePercent = .2f;
+    float sfxVolumePercent = 1;
+    float musicVolumePercent = 1f;
+
+    AudioSource[] musicSources;
+    int activeMusicSourceIndex;
+
+    
+
+    public void PlaySound(AudioClip clip, Vector3 pos)
+    {
+        if (clip != null)
+        {
+            AudioSource.PlayClipAtPoint(clip, pos, sfxVolumePercent * masterVolumePercent);
+        }
+    }
+
+    
+}
---
+++ Gasper/Assets/Scripts/Menu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+    public GameObject mainMenuHolder;
+    public GameObject optionsMenuHolder;
+    public Slider[] volumeSliders;
+   
+
+
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsMenu()
+    {
+        mainMenuHolder.SetActive(false);
+        optionsMenuHolder.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        mainMenuHolder.SetActive(true);
+        optionsMenuHolder.SetActive(false);
+    }
+
+    public void SetMusicVolume()
+    {
+       
+    }
+}
