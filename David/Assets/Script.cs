5ab609ffcb0aa70b8cc2b56cba45918a49c62000
daavidek008 <daavidtkalec@gmail.com>
2016-11-07 20:06:15 +0100

    Add files via upload

---
+++ David/Assets/Script.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Script : MonoBehaviour {
+	public GameObject MainGameObject;
+	public GameObject StatistikaGameObject;
+
+
+	public void StatistikaMenu(){
+		MainGameObject.SetActive (false);
+		StatistikaGameObject.SetActive (true);
+	}
+
+	public void MainMenu(){
+		MainGameObject.SetActive (true);
+		StatistikaGameObject.SetActive (false);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+}
