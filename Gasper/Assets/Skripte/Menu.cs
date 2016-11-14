49fa29d191f448ba810f88e8ca63f1f16d0db122
Gasprck <rencof.gasper@gmail.com>
2016-11-14 22:51:24 +0100

    Add files via upload

---
+++ Gasper/Assets/Skripte/Menu.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject mainMenuHolder;
+    public GameObject optionsMenuHolder;
+
+    public Slider[] volumeSliders;
+    /*public Toggle[] resolutionToggles;
+    public Toggle fullscreenToggle;
+    /*public int[] screenWidths;
+    int activeScreenResIndex;*/
+
+    /*void Start()
+    {
+        activeScreenResIndex = PlayerPrefs.GetInt("screen res index");
+        bool isFullscreen = (PlayerPrefs.GetInt("fullscreen") == 1) ? true : false;
+
+        volumeSliders[0].value = AudioManager.instance.masterVolumePercent;
+        volumeSliders[1].value = AudioManager.instance.musicVolumePercent;
+        volumeSliders[2].value = AudioManager.instance.sfxVolumePercent;
+
+        for (int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].isOn = i == activeScreenResIndex;
+        }
+
+        fullscreenToggle.isOn = isFullscreen;
+    }*/
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void OptionsMenu()
+    {
+        mainMenuHolder.SetActive(false);
+        optionsMenuHolder.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        mainMenuHolder.SetActive(true);
+        optionsMenuHolder.SetActive(false);
+    }
+
+    public void StartLevel()
+    {
+        //Application.LoadLevel(1);
+        SceneManager.LoadScene(1);
+    }
+
+    /*public void SetScreenResolution(int i)
+    {
+        if (resolutionToggles[i].isOn)
+        {
+            activeScreenResIndex = i;
+            float aspectRatio = 16 / 9f;
+            Screen.SetResolution(screenWidths[i], (int)(screenWidths[i] / aspectRatio), false);
+            PlayerPrefs.SetInt("screen res index", activeScreenResIndex);
+            PlayerPrefs.Save();
+        }
+    }
+
+    
+    */
+    public void SetMasterVolume(float value)
+    {
+        //AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
+        //AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        //AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Music);
+    }
+    /*
+    public void SetSfxVolume(float value)
+    {
+        AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Sfx);
+    }*/
+
+}
---
+++ Gasper/Assets/Skripte/premikanjeLika.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class premikanjeLika : MonoBehaviour {
+
+    /*[SerializeField]
+    public float movingSpeed;*/
+
+    [SerializeField]
+    public float jumpingSpeed = 400;
+
+    Rigidbody2D myRigidbody2d;
+
+    void Awake()
+    {
+        myRigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+       // myRigidbody2d.AddForce(Vector2.right * Input.GetAxis("Horizontal") * movingSpeed);
+        Movement();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        myRigidbody2d.AddForce(Vector2.up * jumpingSpeed);
+    }
+
+    void Movement() {
+        /*if (Input.GetKey (KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * 5f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (Input.GetKey (KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.right * 5f * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }*/
+
+        //if (Input.touchCount == 1)
+        if (Input.touchCount > 0)
+        {
+            //var touch = Input.touches[0];
+            var touch = Input.GetTouch(0);
+            if (touch.position.x < Screen.width / 2)
+            {
+                transform.Translate(Vector2.right * 5f * Time.deltaTime);
+                transform.eulerAngles = new Vector2(0, 180);
+            }
+            else if (touch.position.x > Screen.width / 2)
+            {
+                transform.Translate(Vector2.right * 5f * Time.deltaTime);
+                transform.eulerAngles = new Vector2(0, 0);
+            }
+        }
+    }
+}
