6e6da485eb7a32c86e8dc35b8de41c14307fe3cd
Gasprck <rencof.gasper@gmail.com>
2016-11-21 20:28:49 +0100

    Assets

---
+++ Gasper/Assets/Scripts/Platform.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform : MonoBehaviour {
+
+	public BoxCollider2D playerBoxCollider2d;
+
+	public PlatformSpawner platformSpawnerScript;
+
+	public Transform cameraTrans;
+
+	public GameManager gameManagerScript;
+
+	public int platformType;
+
+	BoxCollider2D myBoxCollider2d;
+
+	Rigidbody2D myRigidBody2d;
+
+	float cameraHalfHeight;
+
+
+	void Awake () {
+		myBoxCollider2d = GetComponent<BoxCollider2D> ();
+		if (platformType == 1 || platformType == 2) {
+			myRigidBody2d = GetComponent<Rigidbody2D> ();
+		    }
+	}
+
+	void Start(){
+		cameraHalfHeight = Camera.main.orthographicSize + .5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(platformType){
+		case 0: 
+			if (gameManagerScript.gameRunning) {
+				if (playerBoxCollider2d.bounds.min.y < myBoxCollider2d.bounds.min.y && myBoxCollider2d.isTrigger == false)
+					myBoxCollider2d.isTrigger = true;
+				else if (playerBoxCollider2d.bounds.min.y > myBoxCollider2d.bounds.max.y && myBoxCollider2d.isTrigger == true)
+					myBoxCollider2d.isTrigger = false;
+			}
+			break;
+		case 1:
+		case 2:
+			if (gameManagerScript.gameRunning && myRigidBody2d.isKinematic) {
+				if (playerBoxCollider2d.bounds.min.y < myBoxCollider2d.bounds.min.y && myBoxCollider2d.isTrigger == false)
+					myBoxCollider2d.isTrigger = true;
+				else if (playerBoxCollider2d.bounds.min.y > myBoxCollider2d.bounds.max.y && myBoxCollider2d.isTrigger == true)
+					myBoxCollider2d.isTrigger = false;
+			}
+			break;
+		}
+		if (transform.position.y <= cameraTrans.position.y - cameraHalfHeight)
+			ImDone ();
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (platformType == 1 || platformType == 2) {
+			if (col.gameObject.CompareTag ("Player")) {
+				myRigidBody2d.isKinematic = false;
+				myBoxCollider2d.isTrigger = true;
+			}
+		}
+	}
+
+	void ImDone(){
+		platformSpawnerScript.topPlatformYPos += platformSpawnerScript.distBetween;
+		platformSpawnerScript.SpawnNext ();
+
+		if (platformType == 1 || platformType == 2)
+			myRigidBody2d.isKinematic = true;
+
+		platformSpawnerScript.SendToBin (transform,platformType);
+	}
+}
---
+++ Gasper/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Transform cameraTrans;
+
+    public GameManager gameManagerScript;
+
+    [SerializeField]
+    AudioSource myAudioSource;
+    [SerializeField]
+    AudioClip jumpSound;
+
+    [SerializeField]
+    Vector3 firstPos;
+
+    [SerializeField]
+    float playerFirstYPos;
+
+    [SerializeField]
+    float moveSpd;
+
+    [SerializeField]
+    float jumpPower;
+
+    Rigidbody2D myRigidbody2d;
+    WaitForSeconds waitFallSound;
+
+    public bool isDead = false;
+
+    void Awake() {
+        myRigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Start() {
+        StartCoroutine(ImAlive());
+    }
+
+    public void Reset(){
+        myRigidbody2d.velocity = Vector3.zero;
+        transform.position = firstPos;
+    }
+
+    void Update() {
+        if (gameManagerScript.gameRunning) {
+            kontroleTouch();
+            CheckIfDead();
+        }
+    }
+
+    void FixedUpdate() {
+        if (gameManagerScript.gameRunning)
+            Movement();
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+         if (col.gameObject.CompareTag("Jumpable")) {
+            myRigidbody2d.velocity = new Vector2(myRigidbody2d.velocity.x, 0f);
+            myRigidbody2d.AddForce(Vector2.up * jumpPower);
+            PlaySound(jumpSound);
+        }
+    }
+
+    int TouchInput() {
+        #if UNITY_EDITOR
+        if (Application.isPlaying)
+            return Mathf.RoundToInt(Input.GetAxisRaw("Horizontal"));
+        #endif
+        return 0;
+    }
+
+    void Movement() {
+        myRigidbody2d.AddForce(Vector2.right * TouchInput() * moveSpd);
+        float maxXVel = Mathf.Clamp(myRigidbody2d.velocity.x, -5f, 5f);
+        myRigidbody2d.velocity = new Vector2(maxXVel, myRigidbody2d.velocity.y);
+    }
+
+    void kontroleTouch()  {
+        if (TouchInput() < 0 && transform.localScale.x > 0)
+            transform.localScale = Vector3.one - (Vector3.right * 2);
+        else if (TouchInput() > 0 && transform.localScale.x < 0)
+            transform.localScale = Vector3.one;
+    }
+
+    void CheckIfDead() {
+        if (transform.position.y <= cameraTrans.position.y - 8f && !isDead) {
+            isDead = true;
+            StartCoroutine(ImDead());
+        }
+    }
+
+    IEnumerator ImDead()    {
+        yield return waitFallSound;
+
+        gameManagerScript.GameOver();
+
+        yield return waitFallSound;
+        yield return waitFallSound;
+        yield return waitFallSound;
+
+        gameManagerScript.UIRestart(true);
+    }
+
+    IEnumerator ImAlive()    {
+
+        yield return waitFallSound;
+
+        Reset();
+
+        yield return waitFallSound;
+        yield return waitFallSound;
+        yield return waitFallSound;
+
+        gameManagerScript.UIRestart(false);
+    }
+
+    void PlaySound(AudioClip clipToPlay)
+    {
+        myAudioSource.clip = clipToPlay;
+        myAudioSource.Play();
+    }
+
+
+}
