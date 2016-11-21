b784acd37fb088754d40d3c92a25a70aff75f898
daavidek008 <daavidtkalec@gmail.com>
2016-11-21 20:28:51 +0100

    Add files via upload

---
+++ Assets/Scripts/sledenjeKamere.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sledenjeKamere : MonoBehaviour {
+
+	[SerializeField]
+	managerIgre gameManagerScript;
+
+	public Transform player;
+
+	Vector3 startPos;
+	Vector3 followPos;
+	float yPos;
+
+	void Awake(){
+
+		followPos = new Vector3 (transform.position.x, 0, transform.position.z);
+		startPos = transform.position;
+	}
+
+	void Start(){
+
+		ResetCam ();
+
+	}
+
+	void Update () {
+	
+		if (gameManagerScript.gameRunning)
+			Follow ();
+
+	}
+
+	void Follow(){
+
+		yPos = Mathf.Max (yPos, player.position.y);
+
+		transform.position = followPos + (Vector3.up * yPos);
+
+	}
+
+	public void ResetCam(){
+
+		transform.position = startPos;
+		yPos = startPos.y;
+	}
+}
